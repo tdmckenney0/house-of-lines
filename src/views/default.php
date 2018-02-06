d624f1c9a99ed7e9ea1d2d24c259a490dc4083c1
Tanner Mckenney <tmckenney7@outlook.com>
2018-02-05 23:00:13 -0800

    Added Figures and UI.

--- src/views/default.php
+++ src/views/default.php
@@ -1,12 +1,50 @@
 <!doctype html>
 <html>
 	<head>
-		<script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+		<?php ob_start(); $solutions[] = new Solution(false); ob_end_clean(); ?>
 
+		<script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
 		<link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" />
 	</head>
 	<body>
-		<h1>House of Lines</h1>
-		<pre><?php print_r((new Solution(false))->path, true); ?></pre>
+		<div class="container">
+			<div class="row">
+				<div class="col-3">
+					<figure class="figure">
+						<img src="src/views/img/fig1.jpg" class="figure-img img-fluid rounded" alt="figure 1">
+						<figcaption class="figure-caption">Figure 1</figcaption>
+					</figure>
+
+					<figure class="figure">
+						<img src="src/views/img/fig2.jpg" class="figure-img img-fluid rounded" alt="figure 1">
+						<figcaption class="figure-caption">Figure 2</figcaption>
+					</figure>
+				</div>
+
+				<div class="col">
+					<h1 class="display-1">House of Lines</h1>
+					<table class="table">
+						<thead>
+							<tr>
+								<th>Solution #</th>
+								<th>Path</th>
+								<th>Allow Center Pivot?</th>
+								<th>Time Taken (μs)</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php foreach($solutions as $solution): ?>
+								<tr>
+									<td>1</td>
+									<td><?php echo implode('->', $solution->path); ?></td>
+									<td><?php echo ($solution->allowCenter ? 'Yes' : 'No'); ?></td>
+									<td><?php echo floor($solution->timeTaken * 1000000); ?>μs</td>
+								</tr>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
